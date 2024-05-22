61a1155d6ae4b0b1a0021c48f568f0f60b72dae0
Advika Gupta <117989019+advika30@users.noreply.github.com>
2024-05-22 19:04:47 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,70 @@
+//driver
+package oddeven;
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.fs.Path;
+public class driver
+{
+     public static void main(String args[]) throws IOException
+     {
+        JobConf conf=new JobConf(driver.class);
+        conf.setMapperClass(mapper.class);
+        conf.setReducerClass(reducer.class);
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(conf,new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf,new Path(args[1]));
+        JobClient.runJob(conf);
+     }
+}
+
+
+//mapper
+package oddeven;
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.io.*;
+public class mapper extends MapReduceBase Mapper(LongWritable,Text,Text,IntWritable)
+{
+    public void map(LongWritable key,Text value,OutputCollector<Text,IntWritable> output,Reporter r) throws IOException
+    {
+        String[] line=value.toString.split(" ");
+        for(String num:line)
+        {
+            int n=Integer.parseInt(num);
+            if(n%2==0)
+            {
+                output.collect(new Text("even"),new IntWritable(n));
+            }
+            else
+            {
+                output.collect(new Text("odd"),new IntWritable(n));
+            }
+        }
+    }
+}
+
+//reducer
+
+package oddeven;
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.io.*;
+public class reducer extends MapReduceBase Reducer(Text,IntWritable,Text,IntWritable)
+{
+    public void reduce(Text key,Iterator<IntWritable> value,OutputCollector<Text,IntWritable> output,Reporter r) throws IOException
+    {
+          int s=0,c=0;
+          while(value.hasNext())
+          {
+            s+=value.next().get();
+            c++;
+          }
+          output.collect(new Text("sum is"),new IntWritable(s));
+          output.collect(new Text("count is"),new IntWritable(c));
+    }
+}
